230b089a53568bf1fc75e2eb0ebf106aaa8be79b
Ruslan Talpa <ruslan.talpa@gmail.com>
2017-11-13 10:14:21 +0200

    merge latest changes

--- resources/build.sh
+++ resources/build.sh
@@ -22,19 +22,8 @@ cat dist/*.css > graphiql.css
 echo "Bundling subzero_graphiql.js..."
 browserify -g browserify-shim -s subZeroGraphiQL dist/subzero_index.js > subzero_graphiql.js
 echo "Bundling subzero_graphiql.min.js..."
-browserify -g browserify-shim -g uglifyify -s subZeroGraphiQL dist/subzero_index.js 2> /dev/null | uglifyjs -c --screw-ie8 > subzero_graphiql.min.js 2> /dev/null
+browserify -g browserify-shim -t uglifyify -s subZeroGraphiQL dist/subzero_index.js 2> /dev/null | uglifyjs -c > subzero_graphiql.min.js 2> /dev/null
 echo "Bundling subzero_graphiql.css..."
-postcss --no-map --use autoprefixer -d dist/ css/*.css
-cat dist/*.css > subzero_graphiql.css
-
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
-
-# stage any changes and new files
-git add -A
-# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "Deploy to GitHub [ci skip]"
-# and push, but send any output to /dev/null to hide anything sensitive
-git push --quiet origin master
+cp graphiql.css subzero_graphiql.css
 
 echo "Done"
